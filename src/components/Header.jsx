0e5308edac1edbd0d4160bad756e7a0b0e2d342d
sekai0330 <asamieto965@gmail.com>
2023-11-04 23:08:29 -0600

    ajustes de navegacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,11 +33,11 @@ export function Header() {
         </button>
         <div
           className={`absolute top-full left-0 px-1 pb-1 w-full md:w-auto ${
-            isMenuOpen ? 'block bg-grisoscuro' : 'hidden'
+            isMenuOpen ? 'block bg-red-600' : 'hidden'
           } transition-all duration-500 ease-in-out md:flex md:items-center md:space-x-8 md:relative md:bg-transparent`}
           id="navbar-default"
         >
-          <ul className="text-sm md:text-sm lg:text-sm 2xl:text-lg flex flex-col p-4 md:p-0 mt-4 border border-naranja700 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          <ul className="text-sm md:text-sm lg:text-sm 2xl:text-lg flex flex-col p-4 md:p-0 mt-4 border border-red-600 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
               <a href='#hombres' className="block py-2 pl-3 pr-4 text-white rounded transition duration-300 hover:bg-red-800 md:p-1">
                 Tienda
